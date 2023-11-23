7638affc5a7e3d192f7e31bc564ad2d77a48b04a
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-11-23 13:59:40 +0530

    Old vs Enhanced Switch-Case Statement with documentation

--- src/com/neelesh/enhanced_switch/EnhancedSwitchStatement.java
+++ src/com/neelesh/enhanced_switch/EnhancedSwitchStatement.java
@@ -7,10 +7,10 @@ public class EnhancedSwitchStatement {
         System.out.print("Enter day: ");
         int day = new Scanner(System.in).nextInt();
 
-/*
-        Old-fashioned switch-case statement
-*/
-        switch(day % 7){
+        /*
+         * Old-fashioned switch-case statement
+         */
+        switch (day % 7) {
             case 1:
                 System.out.println("Weekday");
                 break;
@@ -34,10 +34,10 @@ public class EnhancedSwitchStatement {
                 break;
         }
 
-/*
-        Old-fashioned switch-case statement with reduced redundancy
-*/
-        switch(day % 7){
+        /*
+         * Old-fashioned switch-case statement with reduced redundancy
+         */
+        switch (day % 7) {
             case 1:
             case 2:
             case 3:
@@ -51,10 +51,10 @@ public class EnhancedSwitchStatement {
                 break;
         }
 
-/*
-    * Enhanced switch-case statement
-    * Introduced in Java 12 version under the name "switch expressions"
-*/
+        /*
+         * Enhanced switch-case statement
+         * Introduced in Java 12 version under the name "switch expressions"
+         */
         switch (day % 7) {
             case 1 -> System.out.println("Weekday");
             case 2 -> System.out.println("Weekday");
@@ -65,13 +65,72 @@ public class EnhancedSwitchStatement {
             case 0 -> System.out.println("Weekend");
         }
 
-/*
-        Old-fashioned switch-case statement with reduced redundancy
-*/
-        switch(day % 7){
+        /*
+         * Enhanced switch-case statement with reduced redundancy
+         */
+        switch (day % 7) {
             case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
             case 6, 0 -> System.out.println("Weekend");
         }
 
+
+
+        /*
+         * Enhanced switch-case statement
+         * Introduced in Java 13 version, an extension to Java 12, allowing expressions to return a value using "yield" keyword
+         * Rules:
+         * 1. default case is mandatory even though the results are bounded inorder to prevent code crash
+         * 2. yield should be enclosed with in code blocks or braces
+         * 3. Return type of switch should match with the datatype of the variable
+         * 4. If we don't save the yielding result in a variable, we get error saying "yield outside of switch Expression.
+         * 5. If we use "var" then yielding values can be of any type as shown below because Compiler infers the type during compilation
+         * 6. Switch expression with yielding results is used in variable initialization. So make sure to use semicolon ';' at the end of switch expression
+         */
+        var varDayName = switch (day % 7) {
+            case 1 -> {
+                System.out.println("Weekday");
+                yield 1;
+            }
+            case 2 -> {
+                System.out.println("Weekday");
+                yield 2.0;
+            }
+            case 3 -> {
+                System.out.println("Weekday");
+                yield "Wednesday";
+            }
+            case 4 -> {
+                System.out.println("Weekday");
+                yield "Thursday";
+            }
+            case 5 -> {
+                System.out.println("Weekday");
+                yield "Friday";
+            }
+            case 6 -> {
+                System.out.println("Weekend");
+                yield 'S';
+            }
+            default -> {
+                System.out.println("Weekend");
+                yield true;
+            }
+        };
+
+        System.out.println("Day = " + varDayName);
+
+        /*
+         * Enhanced switch-case statement with reduced redundancy
+         */
+        String dayName = switch (day % 7) {
+            case 1, 2, 3, 4, 5 -> {
+                yield "Weekday";
+            }
+            default -> {
+                yield "Sunday";
+            }
+        };
+
+        System.out.println("Day = " + dayName);
     }
 }
